c981c3bfda9fd6a3a9c7e621bf98d8867f225a41
Jared Chapman <thejaredchapman@gmail.com>
2022-06-12 21:23:00 -0500

    Add files via upload
    ETL files from s3 and put back to another s3 bucket.

---
+++ tests/custom_exceptions.py
@@ -0,0 +1,9 @@
+"""Custom Exceptions"""
+
+class WrongFormatException(Exception):
+    """
+    WrongFormatException class
+
+    Exception that can be raised when the format type
+    given as paramter is not supported.
+    """
